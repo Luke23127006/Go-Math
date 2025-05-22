f251e38e8347d35d028190328377638246ff87b4
Tran Nguyen Khai Luan <tnkluan23@clc.fitus.edu.vn>
2025-05-22 12:16:09 +0700

    update Luke.go

--- Math/Luke.go
+++ Math/Luke.go
@@ -7,3 +7,14 @@ func Add(a, b int) int {
 func Subtract(a, b int) int {
 	return a - b
 }
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) int {
+	if b == 0 {
+		panic("division by zero")
+	}
+	return a / b
+}
